ea02c4021261d0acc27be8d4bc503b96863235f3
nneha08 <nnehagrwl@gmail.com>
2017-06-17 14:36:12 +0530

    Initial commit

---
+++ rango/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import rango
+from django.shortcuts import render
+from rango.models import Category
+def index(request):
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories' : category_list}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "it will tell u about RANGO :)"}
+    return render(request, 'rango/about.html', context_dict)
