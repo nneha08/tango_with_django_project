7993fa8bc53a0e5a9bb5efdaf504479a3b58725f
nneha08 <nnehagrwl@gmail.com>
2017-06-18 11:29:44 +0530

    Updated Models, Views and templates to show different categories on home page.

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 import rango
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
+
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories' : category_list}
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -15,5 +17,21 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+
     context_dict = {'boldmessage': "it will tell u about RANGO :)"}
     return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    url(r'^rango/', include('rango.urls')),  # ADD THIS NEW TUPLE!
 )
 if settings.DEBUG:
     urlpatterns += patterns(
